4acde225cb2d198ed06785dd0a1cb87278b2ff94
Kai-z3 <Kai.Knezevic@bbz-sh.ch>
2026-01-17 09:43:59 +0100

    sub

--- main.cpp
+++ main.cpp
@@ -20,7 +20,16 @@ int main() {
             break;
         case 2:
             cout << "Gruppenmitglied 1 kann hier die Subtraktion implementieren."  << endl;
-            // hier Funktion für die Subtraktion einfügen
+        double zahl1sub, zahl2sub, ergebnissub;
+        cout << "Subtraktion" << endl;
+        cout << "Gib die erste Zahl ein: ";
+        cin >> zahl1sub;
+        cout << "Gib die zweite Zahl ein: ";
+        cin >> zahl2sub;
+        ergebnissub = zahl1sub - zahl2sub;
+        cout << "Ergebnis:" << zahl1sub << "-" << zahl2sub << "= " << ergebnissub << endl;
+
+
             break;
         case 3:
             cout << "Gruppenmitglied 1 kann hier die Multiplikation implementieren."  << endl;
@@ -28,8 +37,16 @@ int main() {
             break;
         case 4:
             cout << "Gruppenmitglied 1 kann hier die Division implementieren."  << endl;
-            // hier die Funktion für die Division einfügen
+        double zahl1div, zahl2div, ergebnisdiv;
+        cout << "Division" << endl;
+        cout << "Gib die erste Zahl ein: ";
+        cin >> zahl1div;
+        cout << "Gib die zweite Zahl ein: ";
+        cin >> zahl2div;
+        ergebnisdiv = zahl1div / zahl2div;
+        cout << "Ergebnis:" << zahl1div << "/" << zahl2div << "= " << ergebnisdiv << endl;
             break;
+
         default:
             cout << "Falsche Auswahl, das Programm wird beendet."  << endl;
             break;
